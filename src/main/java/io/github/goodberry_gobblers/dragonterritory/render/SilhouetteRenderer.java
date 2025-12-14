5f9593c848c185b9c5701eb43d39f326f6f4d0e1
candyCalc <calansk0@gmail.com>
2025-12-14 06:07:02 -0800

    Change the curse dragons follow to be elliptical

--- src/main/java/io/github/goodberry_gobblers/dragonterritory/render/SilhouetteRenderer.java
+++ src/main/java/io/github/goodberry_gobblers/dragonterritory/render/SilhouetteRenderer.java
@@ -4,15 +4,14 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import io.github.goodberry_gobblers.dragonterritory.DragonTerritory;
 import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.FogRenderer;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
-import org.joml.Matrix4f;
-import org.joml.Vector3f;
+import org.joml.*;
 
+import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -52,6 +51,9 @@ public class SilhouetteRenderer {
                     silhouette.size,
                     silhouette.offsetAngle,
                     silhouette.radius,
+                    silhouette.ellipsesRotation,
+                    silhouette.ellipticalWidth,
+                    silhouette.centerOffset,
                     silhouette.height,
                     silhouette.blocksPerSecond,
                     silhouette.clockwise,
@@ -63,21 +65,6 @@ public class SilhouetteRenderer {
         BufferUploader.drawWithShader(bufferbuilder.end());
 
         poseStack.popPose();
-    }
-
-    public static void renderAt(BufferBuilder bufferbuilder, Matrix4f matrix4f, int size, float angleOffset, float radius, float height, float blocksPerSecond, boolean clockwise, Vector3f color, int renderTick, float partialTick) {
-        double angle = Math.toRadians(angleOffset) + (clockwise ? -1 : 1) * (blocksPerSecond / radius) * (renderTick + partialTick) / 20;
-        float cos = (float) Math.cos(angle);
-        float sin = (float) Math.sin(angle);
-        int frameId = getFrameId(size, blocksPerSecond, angleOffset, renderTick);
-
-        float uvy1 = (frameId+1)/3f;
-        float uvy2 = frameId/3f;
-
-        bufferbuilder.vertex(matrix4f, (radius-3*size)*cos+2*size*sin, height, (radius-3*size)*sin-2*size*cos).uv(0f, clockwise ? uvy2 : uvy1).color(color.x, color.y, color.z, 1f).endVertex();
-        bufferbuilder.vertex(matrix4f, (radius+3*size)*cos+2*size*sin, height-radius/30, (radius+3*size)*sin-2*size*cos).uv(1f, clockwise ? uvy2 : uvy1).color(color.x, color.y, color.z, 1f).endVertex();
-        bufferbuilder.vertex(matrix4f, (radius+3*size)*cos-2*size*sin, height-radius/30, (radius+3*size)*sin+2*size*cos).uv(1f, clockwise ? uvy1 : uvy2).color(color.x, color.y, color.z, 1f).endVertex();
-        bufferbuilder.vertex(matrix4f, (radius-3*size)*cos-2*size*sin, height, (radius-3*size)*sin+2*size*cos).uv(0f, clockwise ? uvy1 : uvy2).color(color.x, color.y, color.z, 1f).endVertex();
     }
 
     private static int getFrameId(int size, float blocksPerSecond, float offset, int renderTick) {
@@ -92,6 +79,51 @@ public class SilhouetteRenderer {
         // toss that ^^^^ into desmos and add the variables 'f' and 'c' for "frequency" and "count".
         double thing = Math.sin(x * Math.PI/4);
         return x % (4 * flapFrequency) < 4 * flapCount ? (int) Math.round(2 * thing * thing) : 0;
+    }
+
+    public static void renderAt(BufferBuilder bufferbuilder, Matrix4f matrix4f, int size, float angleOffset, float radius, float ellipsesRotation, float ellipticalWidth, Vector2f centerOffset, float height, float blocksPerSecond, boolean clockwise, Vector3f color, int renderTick, float partialTick) {
+        double angle = Math.toRadians(angleOffset) + (clockwise ? -1 : 1) * (blocksPerSecond / radius) * (renderTick + partialTick) / 20;
+        int frameId = getFrameId(size, blocksPerSecond, angleOffset, renderTick);
+
+
+        for (int i = 0; i < 4; ++i) {
+            boolean isInterior = i % 3 == 0; // if the vertex is facing towards the center or away
+            boolean isFront = i >= 2; //if the vertex is at the front
+            double sin = Math.sin(angle);
+            double cos = Math.cos(angle);
+
+            Vector2f dragonDirection = rotate(-ellipticalWidth * sin, (float) cos, ellipsesRotation);
+            Vector2f pos = rotate(3 * size * (isInterior ? -1 : 1), 2 * size * (isFront ? 1 : -1), Math.atan2(dragonDirection.y, dragonDirection.x) - Math.PI/2);
+            Vector2f offset = rotate(ellipticalWidth * radius * cos, radius * sin, ellipsesRotation);
+
+            bufferbuilder.vertex(
+                    matrix4f, //matrix
+                    pos.x + offset.x + centerOffset.x,
+                    isInterior ? height : height-radius/30, // height. exterior vertices are pushed down to reduce grazing angles
+                    pos.y + offset.y + centerOffset.y
+            ).uv(
+                    isInterior ? 0f : 1f,
+                    clockwise ^ isFront ? frameId/3f : (frameId+1)/3f //flips the dragon's uv if it's going in the opposite direction
+            ).color(
+                    color.x,
+                    color.y,
+                    color.z,
+                    1f
+            ).endVertex();
+        }
+
+        //bufferbuilder.vertex(matrix4f, (radius-3*size)*cos+2*size*sin, height, (radius-3*size)*sin-2*size*cos).uv(0f, clockwise ? uvy2 : uvy1).color(color.x, color.y, color.z, 1f).endVertex();
+        //bufferbuilder.vertex(matrix4f, (radius+3*size)*cos+2*size*sin, height-radius/30, (radius+3*size)*sin-2*size*cos).uv(1f, clockwise ? uvy2 : uvy1).color(color.x, color.y, color.z, 1f).endVertex();
+        //bufferbuilder.vertex(matrix4f, (radius+3*size)*cos-2*size*sin, height-radius/30, (radius+3*size)*sin+2*size*cos).uv(1f, clockwise ? uvy1 : uvy2).color(color.x, color.y, color.z, 1f).endVertex();
+        //bufferbuilder.vertex(matrix4f, (radius-3*size)*cos-2*size*sin, height, (radius-3*size)*sin+2*size*cos).uv(0f, clockwise ? uvy1 : uvy2).color(color.x, color.y, color.z, 1f).endVertex();
+    }
+
+    // I should organize this better
+    private static Vector2f rotate(double x, double y, double angle) {
+        return new Vector2f(
+                (float) (x * Math.cos(angle) - y * Math.sin(angle)),
+                (float) (x * Math.sin(angle) + y * Math.cos(angle))
+        );
     }
 
     private static int dragonCount(double distance) {
@@ -114,7 +146,10 @@ public class SilhouetteRenderer {
             list.add(new Silhouette(
                     randomSource.nextIntBetweenInclusive(2, 4),
                     randomSource.nextFloat() * 360,
-                    randomSource.nextIntBetweenInclusive(20, 150),
+                    randomSource.nextIntBetweenInclusive(30, 75),
+                    randomSource.nextFloat() * 360,
+                    randomSource.nextFloat() + 1,
+                    new Vector2f(randomSource.nextFloat()*20-10, randomSource.nextFloat()*20-10),
                     randomSource.nextIntBetweenInclusive(20, 60),
                     randomSource.nextIntBetweenInclusive(10, 30),
                     randomSource.nextBoolean()
@@ -128,14 +163,20 @@ public class SilhouetteRenderer {
         public int size;
         public float offsetAngle;
         public float radius;
+        public float ellipsesRotation;
+        public float ellipticalWidth;
+        public Vector2f centerOffset;
         public float height;
         public float blocksPerSecond;
         public boolean clockwise;
 
-        public Silhouette(int size, float offsetAngle, float radius, float height, float blocksPerSecond, boolean clockwise) {
+        public Silhouette(int size, float offsetAngle, float radius, float ellipsesRotation, float ellipticalWidth, Vector2f centerOffset, float height, float blocksPerSecond, boolean clockwise) {
             this.size = size;
             this.offsetAngle = offsetAngle;
             this.radius = radius;
+            this.ellipsesRotation = ellipsesRotation;
+            this.ellipticalWidth = ellipticalWidth;
+            this.centerOffset = centerOffset;
             this.height = height;
             this.blocksPerSecond = blocksPerSecond;
             this.clockwise = clockwise;
